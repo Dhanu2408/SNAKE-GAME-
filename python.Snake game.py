694abbd4c67d42994fce0d951d6d6d1a3bad7d08
Dhanu2408 <rajesh3180raj@gmail.com>
2025-06-29 20:04:08 +0530

    Add files via upload

---
+++ python.Snake game.py
@@ -0,0 +1,53 @@
+import pygame
+import time
+import random
+
+# Initialize pygame
+pygame.init()
+
+# Screen dimensions
+width = 600
+height = 400
+
+# Colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
+# Create game window
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Snake Game')
+
+# Clock to control speed
+clock = pygame.time.Clock()
+snake_block = 10
+snake_speed = 15
+
+# Font styles
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 25)
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(screen, black, [x[0], x[1], snake_block, snake_block])
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    screen.blit(mesg, [width / 6, height / 3])
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = width / 2
+    y1 = height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_List = []
+    Length_of_snake = 1
+
+    foodx = round
